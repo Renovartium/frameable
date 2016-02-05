d24b9efa43e7875cad28fa98b631e76fb3f97d4c
alexrybrown <alexrybrown@gmail.com>
2016-02-04 17:51:50 -0700

    Style options.

--- frameable/urls.py
+++ frameable/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', 'accounts.views.index', name='index'),
     url('', include('django.contrib.auth.urls', namespace='auth')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
-)
+]
